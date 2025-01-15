1e30893a230829c2e625f9bbc4b6d3a7913bef64
mnaid <mnaid@NAIDIRAH>
2025-01-15 18:30:01 -0500

    Add project files.

---
+++ Checkpoint2.14.cpp
@@ -0,0 +1,34 @@
+/* Program File Name: Checkpoint2.14
+Programmer: Naidirah Muhammad
+Date: January 2025
+Requirements:
+Write a program that has the following character variables:
+first, middle, and last. Store your initials in these variables
+then display them on the screen.
+*/
+
+#include <iostream>
+
+
+
+int main()
+{
+	char firstInitial;
+	char middleInitial;
+	char lastInitial;
+	std::cout << "Please enter the initial of your first name:";
+	std::cin >> firstInitial;
+	std::cout << "Please enter the initial of your middle name:";
+	std::cin >> middleInitial;
+	std::cout << "Please enter the initial of your last name:";
+	std::cin >> lastInitial;
+	std::cout << "Your initals are:" << firstInitial << middleInitial << lastInitial;
+
+}
+
+
+
+
+
+
+
